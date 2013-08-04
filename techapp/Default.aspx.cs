c31ccc9cd4ef98648f478f461440cbe10ebfc808
HgNeoh <nhg_neoh@hotmail.com>
2013-08-04 15:57:36 +0800

    testing commit

---
+++ techapp/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace facerecog
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { }
+
+        protected void testButton_Click(object sender, EventArgs e)
+        { testButton.Text = "Changed"; }
+    }
+}
