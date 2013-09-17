63d776266e43d87f6957c77f27423577dd855287
HgNeoh <nhg_neoh@hotmail.com>
2013-09-17 16:26:40 +0800

    Signed-off-by: HgNeoh <nhg_neoh@hotmail.com>

---
+++ Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { updateDateTime(); }
+        protected void showTimeButton_Click(object sender, EventArgs e)
+        { updateDateTime(); }
+        private void updateDateTime()
+        { timeLabel.Text = System.DateTime.Now.Date.ToLongDateString() + " " + System.DateTime.Now.TimeOfDay.ToString(); }
+    }
+}
---
+++ obj/Release/Package/PackageTmp/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication2.Default" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>FaceRecog Main Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="timeLabel" runat="server" /><br /><br />
+        <asp:Button ID="showTimeButton" runat="server" Text="Update Time" OnClick="showTimeButton_Click" />
+    </div>
+    </form>
+</body>
+</html>
